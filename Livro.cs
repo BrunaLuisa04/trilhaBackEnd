9188ad11fd03fe4d0c49c18634a2aeb256b6b8bb
Bruna Luisa <95708140+BrunaLuisa04@users.noreply.github.com>
2022-04-26 15:45:27 -0300

    Add files via upload
    Enviando Exercicio2-Desafio 3

---
+++ Livro.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Desafio3_Livro
+{
+    public class Livro
+    {
+        
+
+        public string Nome {get; set;}
+        public string Autor {get; set;}
+        public string Genero { get; set;}
+        public int  QtdPaginas { get; set;}
+        public int PaginasLidas { get; set; }
+        public int Porcentagem { get; set; }
+
+        //CONSTRUTOR
+        public Livro()
+        {
+            return;
+        }
+
+        public Livro(string titulo, int paginas)
+        {
+            Nome = titulo;
+            QtdPaginas = paginas;
+           
+
+        }
+
+        public void VerificarProgresso()
+        {
+            Porcentagem = PaginasLidas *100 /QtdPaginas;
+            Console.WriteLine(Porcentagem);
+          
+        }
+    }
+
+
+
+
+
+
+}
+
