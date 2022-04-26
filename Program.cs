02d50b1488283249c7818c740be9d634485da632
Bruna Luisa <95708140+BrunaLuisa04@users.noreply.github.com>
2022-04-26 15:49:27 -0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,59 +1,39 @@
-﻿using Desafio3_Livro;
+﻿using Desafio3_Retangulo;
+using System;
 using System.Globalization;
 
-namespace Exercicio_2
+namespace Exercicio_1
 {
     public class Program
 
-    {   //Variáveis pra armazenar as respostas do usuário
-        private static string livro;
-        private static string autor;
-        private static string genero;
-        private static string titulo;
-        private static int paginas;
-
-
+    {
+        //Declaração das variáveis da  minha classe construtora
+        private static double Lado1, Lado2;
         static void Main(string[] args)
         {
-
             //Solicita ao usuário
-            Console.WriteLine("Olá! Digite a seguir informações sobre o livro que está lendo:");
-
-            Console.Write("Nome do livro: ");
-            livro = Console.ReadLine();
-
-            Console.Write("Nome do autor: ");
-            autor = Console.ReadLine();
-
-            Console.Write("Gênero: ");
-            genero = Console.ReadLine();
-
-            Console.WriteLine("Quantas páginas o livro possui: ");
-            paginas = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Digite o primeiro valor para o retângulo");
+            Lado1 = double.Parse(Console.ReadLine());
+            Console.WriteLine("Digite o segundo valor para o retângulo");
+            Lado2 = double.Parse(Console.ReadLine());
 
             //Instanciação-Chama o método construtor da classe
-            Livro novoLivro = new Livro(titulo, paginas);
+            Retangulo novoRetangulo = new Retangulo(Lado1, Lado2);
+            
+            //Chamando os métodos
+            novoRetangulo.CalcularArea();
+            novoRetangulo.CalcularPerimetro();
 
-            novoLivro.Nome = livro;
-            novoLivro.Autor = autor;
-            novoLivro.Genero = genero;
+            
 
-            //Verifica progresso
-            Console.Write("Páginas lidas: ");
-            novoLivro.PaginasLidas = Convert.ToInt32(Console.ReadLine());
-            novoLivro.VerificarProgresso();
-            Console.Write("Páginas lidas: ");
-            novoLivro.PaginasLidas = Convert.ToInt32(Console.ReadLine());
-            novoLivro.VerificarProgresso();
-            Console.Write("Páginas lidas: ");
-            novoLivro.PaginasLidas = Convert.ToInt32(Console.ReadLine());
-            novoLivro.VerificarProgresso();
 
             Console.ReadLine();
 
         }
+        
+        
 
-       
-    }
+    }    
 }
+
 
