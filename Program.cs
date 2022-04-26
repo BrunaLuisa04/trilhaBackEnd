b9de8188c8753841a1cd8a62753826e935e49545
Bruna Luisa <95708140+BrunaLuisa04@users.noreply.github.com>
2022-04-26 15:44:19 -0300

    Add files via upload
    Enviando Exercicio1-Desafio 3

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using Desafio3_Retangulo;
+using System;
+using System.Globalization;
+
+namespace Exercicio_1
+{
+    public class Program
+
+    {
+        //Declaração das variáveis da  minha classe construtora
+        private static double Lado1, Lado2;
+        static void Main(string[] args)
+        {
+            //Solicita ao usuário
+            Console.WriteLine("Digite o primeiro valor para o retângulo");
+            Lado1 = double.Parse(Console.ReadLine());
+            Console.WriteLine("Digite o segundo valor para o retângulo");
+            Lado2 = double.Parse(Console.ReadLine());
+
+            //Instanciação-Chama o método construtor da classe
+            Retangulo novoRetangulo = new Retangulo(Lado1, Lado2);
+            
+            //Chamando os métodos
+            novoRetangulo.CalcularArea();
+            novoRetangulo.CalcularPerimetro();
+
+            
+
+
+            Console.ReadLine();
+
+        }
+        
+        
+
+    }    
+}
+
+
---
+++ Retangulo.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Desafio3_Retangulo
+{
+    public class Retangulo
+    {
+        //Atributos para minha classe
+        public double Lado1;
+        public double Lado2;
+        public double Area;
+        public double Perimetro;
+
+        //CONSTRUTOR
+        public Retangulo()
+        {
+            return;
+        }
+        //CONSTRUTOR
+        public Retangulo(double LadoA, double LadoB)
+        {
+            Lado1 = LadoA;
+            Lado2 = LadoB;
+            return;
+
+        }
+
+        //MÉTODO CALCULO DE ÁREA
+        public void CalcularArea()
+        {
+            Area = Lado1 * Lado2;
+            Console.WriteLine(Area);
+            
+        }
+
+        //MÉTODO CALCULO DE PERÍMETRO
+        public void CalcularPerimetro()
+        {
+            Perimetro = 2 * (Lado1 + Lado2);
+            Console.WriteLine(Perimetro);
+            
+        }
+    }
+
+    
+}
